02409d05aad4a497e8c0a4bb996bfe3d8be1e0c4
himanshusalal <himanshusalal517@gmail.com>
2026-02-04 14:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
 
-    function removeColor() {
-      const select = document.getElementById("colorSelect");
-      const selectedIndex = select.selectedIndex;
+   function removeColor() {
+  const select = document.getElementById("colorSelect");
+  const selectedIndex = select.selectedIndex;
 
-      if (selectedIndex !== -1) {
-        select.remove(selectedIndex);
-      }
-    }
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+}
 
+// button ko JS se connect karna (IMPORTANT)
+document
+  .querySelector('input[type="button"]')
+  .addEventListener("click", removeColor);
